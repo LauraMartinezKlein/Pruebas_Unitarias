09196ec90b376ea0154c577fd9eb47c81ce6fd9b
LauraMartinezKlein <44182421+LauraMartinezKlein@users.noreply.github.com>
2019-11-23 10:35:35 -0400

    Add files via upload

---
+++ SI220-Z1/Saludar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SI220_Z1
+{
+    public class Saludar
+    {
+        public string DecirHola()
+        {
+            return "HOLA MUNDO";
+        }
+
+    }
+
+}
